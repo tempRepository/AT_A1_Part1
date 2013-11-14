9632200a38c8a2f879146198c3aa4a49b62e5262
Filip Jerzyna <filipjerzyna@gmail.com>
2013-11-14 13:21:01 +0100

    Backup

---
+++ SimplerRabinCarp/src/pl/lodz/uni/math/Main.java
@@ -0,0 +1,11 @@
+package pl.lodz.uni.math;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimplerEngine engine=new SimplerEngine(Integer.MAX_VALUE/2);
+        engine.searchForPattern("alamakotaalaalafiernfief", "ala");
+
+    }
+
+}
