c245040e38ab165d1076f83097207926586c749b
Filip Jerzyna <filipjerzyna@gmail.com>
2013-11-16 16:56:04 +0100

    Plain copy from simpler Rabin Karp

---
+++ AT_A1_Part4/src/pl/lodz/uni/math/rabinKarp/Engine.java
@@ -0,0 +1,56 @@
+package pl.lodz.uni.math.rabinKarp;
+
+public class Engine {
+    private static int ourModulo = 0;
+
+    public static int getOurModulo() {
+        return ourModulo;
+    }
+
+    public static void setOurModulo(int ourModulo) {
+        Engine.ourModulo = ourModulo;
+    }
+
+    public Engine(int ourModulo) {
+        this.ourModulo = ourModulo;
+    }
+
+    // In this specific example A should be prime number
+    public int computeHash(String myText) {
+        int hash = 0;
+        for (int i = 0; i < myText.length(); i++) {
+            hash += myText.charAt(i) % ourModulo;
+        }
+        return hash;
+    }
+
+    void searchForPattern(String text, String pattern) {
+        int patternHash = computeHash(pattern);
+        System.out.println("Text length " + text.length() + " pattern length "
+                + pattern.length());
+        int falseCounter = 0;
+        int textHash = computeHash(text.substring(0, pattern.length()));
+        for (int i = 0; i < text.length() - pattern.length(); i++) {
+            if (i != 0) {
+                textHash = (textHash - text.charAt(i - 1) % ourModulo)
+                        + text.charAt(i + pattern.length() - 1);
+            }
+            if (textHash == patternHash) {
+                // sprawdzanie rzeczywistej zgodnosci
+                if (text.substring(i, i + pattern.length()).equals(pattern)) {
+                    System.out.println("I found pattern on position: " + i);
+                } else {
+                    falseCounter++;
+                }
+            }
+        }
+        System.out.println("Fake alarms: " + falseCounter);
+    }
+
+    public static void main(String[] args) {
+        Engine engine = new Engine(Integer.MAX_VALUE / 2);
+        engine.searchForPattern("alamakotaalaalafiernfief", "ala");
+
+    }
+
+}
