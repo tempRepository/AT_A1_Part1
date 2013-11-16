f20b28742738b504f24628d3aa401f2cf065dc01
Filip Jerzyna <filipjerzyna@gmail.com>
2013-11-16 14:23:24 +0100

    Naive version with ? and * works correctly but still doesn't support spaces

--- AT_A1_Part1/src/pl/lodz/uni/math/part1/NaivePM.java
+++ AT_A1_Part1/src/pl/lodz/uni/math/part1/NaivePM.java
@@ -7,33 +7,66 @@ public class NaivePM {
         this.text = text;
     }
 
-   public static boolean myEquals(String text, String pattern) {
+    public static boolean myEquals(String text, String pattern) {
+
         boolean equals = true;
         for (int i = 0; i < text.length() && equals; i++) {
-            if ((text.charAt(i) != pattern.charAt(i)) && pattern.charAt(i)!="?".toCharArray()[0]) {
+            if ((text.charAt(i) != pattern.charAt(i))
+                    && pattern.charAt(i) != "?".toCharArray()[0]) {
                 equals = false;
             }
         }
-
         return equals;
+        // doesn't support spaces!
+        /*
+         * int i = 0, j = 0; while (i < text.length() && j < text.length()) { if
+         * (text.charAt(i) != pattern.charAt(j) && pattern.charAt(j) != '?') {
+         * return false; } else { if (pattern.charAt(j) == '?') { if
+         * ((i+1)<text.length()&& (j+1)<pattern.length() &&
+         * text.charAt(i+1)==pattern.charAt(j+1)) { i++; j++; } else { i--; } }
+         * i++; j++; }
+         * 
+         * } return true;
+         */
     }
 
-    public void search(String pattern) {
-        for (int i = 0; i < text.length() - pattern.length() + 1; i++) {
-            
-                if (myEquals(text.substring(i, i + pattern.length()), pattern)) {
-                    System.out.println("I found occurence at position: " + i);
-                }
-            
+    public static boolean search(String pattern, String text, boolean quiet) {
+        boolean flag = false;
 
+        if (!pattern.contains("*")) {
+            for (int i = 0; i < text.length() - pattern.length() + 1; i++) {
+                if (myEquals(text.substring(i, i + pattern.length()), pattern)) {
+                    if (!quiet) {
+                        System.out.println("I found occurence at position: "
+                                + i);
+                    }
+                    flag = true;
+                }
+            }
+        } else {
+            for (int i = 0; i < text.length() - pattern.length() + 1; i++) {
+                String tempPattern = pattern.substring(0, pattern.indexOf("*"));
+                if (text.contains(tempPattern)) {
+                    boolean answer = search(
+                            pattern.substring(pattern.indexOf("*") + 1),
+                            text.substring(text.indexOf(tempPattern)
+                                    + tempPattern.length()), true);
+                    System.out.println("I found occurence at position: " + text.indexOf(tempPattern));
+                    return answer;
+                } else {
+                    return false;
+                }
+            }
         }
+
+        return flag;
     }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        //dopisać zgodność dla ? jako spacji!
-        NaivePM naive = new NaivePM("alamakotaalala");
-        naive.search("a?a");
+        // dopisać zgodność dla ? jako spacji!
+
+        System.out.println(NaivePM.search("a*a", "pppppavgdga", false));
     }
 
 }
