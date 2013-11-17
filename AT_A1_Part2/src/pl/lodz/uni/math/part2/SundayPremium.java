383eb41e73404ef6493cbd70ef4e658719811880
Filip Jerzyna <filipjerzyna@gmail.com>
2013-11-17 13:56:44 +0100

    Seems to work correctly. Needs some opinion from teacher

--- AT_A1_Part2/src/pl/lodz/uni/math/part2/SundayPremium.java
+++ AT_A1_Part2/src/pl/lodz/uni/math/part2/SundayPremium.java
@@ -1,72 +1,109 @@
 package pl.lodz.uni.math.part2;
 
 public class SundayPremium {
-int[] skipTable=new int[256];
-private void createSkipTable(String pattern)
-{
-    for (int i = 0; i < skipTable.length; i++) {
-        skipTable[i]=pattern.length()+1;
-    }
-    
-    for (int i = 0; i < pattern.length(); i++) {
-        skipTable[pattern.charAt(i)]=pattern.length()-i;
-    }
-    
-    // logic for '?' sign
+    static int[] skipTable = new int[256];
 
-  
-}
-
-private static boolean myEquals(String text, String pattern) {
-
-    boolean equals = true;
-    for (int i = 0; i < text.length() && equals; i++) {
-        if ((text.charAt(i) != pattern.charAt(i))
-                && pattern.charAt(i) != "?".toCharArray()[0]) {
-            equals = false;
+    private static void createSkipTable(String pattern) {
+        for (int i = 0; i < skipTable.length; i++) {
+            skipTable[i] = pattern.length() + 1;
         }
-    }
-    return equals;
-}
 
-public static int starCounter(String pattern) {
-    int counter = 0;
-    for (int i = 0; i < pattern.length(); i++) {
-        if (pattern.charAt(i) == '*') {
-            counter++;
+        for (int i = 0; i < pattern.length(); i++) {
+            skipTable[pattern.charAt(i)] = pattern.length() - i;
         }
-    }
-    return counter;
-}
 
-public void search(String text, String pattern)
-{
-    createSkipTable(pattern);
-    int i=0;
-    while (i<=text.length()-pattern.length()) {
-        if (myEquals( text.substring(i, i+pattern.length()), pattern)) {
-            System.out.println("I found occurence at position "+ i);
+    }
+
+    private static boolean myEquals(String text, String pattern) {
+
+        boolean equals = true;
+        for (int i = 0; i < text.length() && equals; i++) {
+            if ((text.charAt(i) != pattern.charAt(i))
+                    && pattern.charAt(i) != "?".toCharArray()[0]) {
+                equals = false;
+            }
         }
-        if (text.length()>(i+pattern.length())) {
-            if (!pattern.contains("?")) {
-                i+=skipTable[text.charAt(i+pattern.length())];
-            } else {
-                if (skipTable[text.charAt(i+pattern.length())] < skipTable['?']) {
-                    i+=skipTable[text.charAt(i+pattern.length())];
+        return equals;
+    }
+
+    public static int starCounter(String pattern) {
+        int counter = 0;
+        for (int i = 0; i < pattern.length(); i++) {
+            if (pattern.charAt(i) == '*') {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public static boolean search(String pattern, String text, Boolean quiet) {
+        boolean flag = false;
+        createSkipTable(pattern);
+        int i = 0;
+        while (i <= text.length() - pattern.length()) {
+            if (myEquals(text.substring(i, i + pattern.length()), pattern)) {
+                if (!quiet) {
+                    System.out.println("I found occurence at position " + i);
+                }
+                flag = true;
+            }
+            if (text.length() > (i + pattern.length())) {
+                if (!pattern.contains("?")) {
+                    i += skipTable[text.charAt(i + pattern.length())];
                 } else {
-                    i+=skipTable['?'];
+                    if (skipTable[text.charAt(i + pattern.length())] < skipTable['?']) {
+                        i += skipTable[text.charAt(i + pattern.length())];
+                    } else {
+                        i += skipTable['?'];
+                    }
+                }
+
+            } else {
+                break;
+            }
+
+        }
+        return flag;
+    }
+
+    public static boolean starSearch(String pattern, String text, boolean quiet) {
+        boolean flag = false;
+        String output = "";
+
+        if (!pattern.contains("*")) {
+            return search(pattern, text, quiet);
+        } else {
+            for (int i = 0; i < text.length()
+                    - (pattern.length() - starCounter(pattern)) + 1; i++) {
+                String tempPattern = pattern.substring(0, pattern.indexOf("*"));
+                if (text.contains(tempPattern)) {
+                    boolean answer = starSearch(
+                            pattern.substring(pattern.indexOf("*") + 1),
+                            text.substring(text.indexOf(tempPattern)
+                                    + tempPattern.length()), true);
+                    if (!quiet) {
+                        output += "I found occurence at position: "
+                                + text.indexOf(tempPattern) + "\n";
+
+                    }
+                    if (answer) {
+                        System.out.println(output);
+                    }
+                    return answer;
+                } else {
+                    return false;
                 }
             }
-           
-        } else {
-            break;
         }
-        
+        if (flag) {
+            System.out.println(output);
+        }
+        return flag;
     }
-}
+
     public static void main(String[] args) {
-        SundayPremium sunday=new SundayPremium();
-        sunday.search("ababccccanan", "anan");
+        System.out.println(SundayPremium.starSearch("ala*ala*ala",
+                "gdrgrdalamakotaalagrgrgrgrgrgalagrgrg", false));
 
     }
 
