5e10ae95aa87aa0e694d039ea24554fe2d655977
Filip Jerzyna <filipjerzyna@gmail.com>
2013-11-23 11:27:34 +0100

    Working version for 1 row

--- AT_A1_Part4/src/pl/lodz/uni/math/rabinKarp/EnginePremium.java
+++ AT_A1_Part4/src/pl/lodz/uni/math/rabinKarp/EnginePremium.java
@@ -2,6 +2,7 @@ package pl.lodz.uni.math.rabinKarp;
 
 public class EnginePremium {
     private static int ourModulo = 0;
+    public static final int SIGMA = 257;
 
     public static int getOurModulo() {
         return ourModulo;
@@ -14,44 +15,88 @@ public class EnginePremium {
     public EnginePremium(int ourModulo) {
         this.ourModulo = ourModulo;
     }
+    
+//computing first hash in a row
+    public int computeHash(int patternSize, int[] tempHashes) {
+        double hash = 0;
 
-
-    public int computeHash(String myText) {
-        int hash = 0;
-        for (int i = 0; i < myText.length(); i++) {
-            hash += myText.charAt(i)*Math.pow(10, (myText.length()-1)-i) % ourModulo;
+        for (int i = 0; i < patternSize; i++) {
+            double a=tempHashes[i];
+            double b=Math.pow(SIGMA, (patternSize - 1) - i);
+         //   hash += tempHashes[i] * (int)Math.pow(SIGMA, (patternSize - 1) - i);
+            hash += (a*b);
         }
-  
-        return hash;
+
+        return (int) (hash%ourModulo);
     }
 
-    void searchForPattern(String text, String pattern) {
-        int patternHash = computeHash(pattern);
-        System.out.println("Text length " + text.length() + " pattern length "
-                + pattern.length());
-        int falseCounter = 0;
-        int textHash = computeHash(text.substring(0, pattern.length()));
-        for (int i = 0; i < text.length() - pattern.length(); i++) {
-            //recomputing the hash in constant time
-            if (i != 0) {
-                int tempA=(int) (textHash-(text.charAt(i-1)*Math.pow(10, pattern.length() -1)));
-                textHash=(int) (10*tempA)+text.charAt(i+pattern.length()-1);
+    public void computeTempHashes(int[] tempHashes, char[][] text,
+            int patternSize) {
+        for (int i = 0; i < text[0].length; i++) {
+            double columnHash = 0;
+            for (int j = 0; j < patternSize; j++) {
+                int a=text[j][i];
+                int b= (int) Math.pow(SIGMA, patternSize - (j + 1));
+                columnHash += (text[j][i] * Math.pow(SIGMA, patternSize - (j + 1))) % ourModulo;
             }
-            if (textHash == patternHash) {
-                // sprawdzanie rzeczywistej zgodnosci
-                if (text.substring(i, i + pattern.length()).equals(pattern)) {
+            tempHashes[i] = (int) columnHash;
+        }
+    }
+
+    void searchForPattern(char[][] text, char[][] pattern) {
+        int falseCounter = 0;
+        int patternLength = pattern[0].length;
+        int columnsQuantity=text[0].length;
+        int[] tempHashes = new int[columnsQuantity];
+        computeTempHashes(tempHashes, text, patternLength);
+        // computing first window hash
+        int tempHash = computeHash(patternLength, tempHashes);
+        // computing pattern hash
+        int[] patternTempHashes = new int[patternLength];
+        computeTempHashes(patternTempHashes, pattern, patternLength);
+        int patternHash = computeHash(patternLength, patternTempHashes);
+
+        // further logic part
+
+        for (int i = 0; i <= text[0].length - patternLength; i++) {
+            if (i != 0) {
+                //recomputing hash
+                //bug!
+                tempHash=((tempHash-(tempHashes[i-1]*(int)Math.pow(SIGMA, patternLength-1)))*SIGMA+tempHashes[i+(patternLength-1)]) % ourModulo;
+            }
+            if (tempHash==patternHash) {
+                // real equality checking
+                boolean identity=true;
+                outer:
+                for (int j = 0; j < patternLength; j++) {
+                    for (int j2 = 0; j2 < patternLength; j2++) {
+                        if (pattern[j][j2]!=text[j][j2+i]) {
+                            identity=false;
+                            break outer;
+                        }
+                    }
+                }
+                if (identity) {
                     System.out.println("I found pattern on position: " + i);
                 } else {
                     falseCounter++;
                 }
             }
+
         }
+
         System.out.println("Fake alarms: " + falseCounter);
     }
+    
+    
 
     public static void main(String[] args) {
         EnginePremium engine = new EnginePremium(Integer.MAX_VALUE / 2);
-        engine.searchForPattern("alamakotaalakota", "ala");
+        char[][] text={{'1','1','2'}, {'3','3','4'}, {'5','5','6'} };
+        char[][] pattern={{'1','2'}, {'3','4'}};
+
+             
+        engine.searchForPattern(text, pattern);
 
     }
 
