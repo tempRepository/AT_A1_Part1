8ada286ba72d4a198fb2d8cb6e6490a72fa379d6
Filip Jerzyna <filipjerzyna@gmail.com>
2013-11-23 14:36:36 +0100

    Works with multiple, reversed, rotated patterns :)

--- AT_A1_Part4/src/pl/lodz/uni/math/rabinKarp/EnginePremium.java
+++ AT_A1_Part4/src/pl/lodz/uni/math/rabinKarp/EnginePremium.java
@@ -1,5 +1,7 @@
 package pl.lodz.uni.math.rabinKarp;
 
+import java.util.HashSet;
+
 public class EnginePremium {
     private static int ourModulo = 0;
     public static final int SIGMA = 257;
@@ -47,57 +49,81 @@ public class EnginePremium {
     }
 
     void searchForPattern(char[][] text, char[][] pattern) {
+        HashSet<char[][]> patterns = new HashSet<>();
+        HashSet<Integer> patternsHashes = new HashSet<>();
         int falseCounter = 0;
         int patternLength = pattern[0].length;
         int columnsQuantity = text[0].length;
-        
+
         // computing pattern hash
-        int[] patternTempHashes = new int[patternLength];
-        computeTempHashes(patternTempHashes, pattern, patternLength);
-        int patternHash = computeHash(patternLength, patternTempHashes);
+        int[] patternTempHashes;
+
+        patterns.add(pattern);
+        pattern = reversePattern(pattern);
+        patterns.add(pattern);
+        for (int i = 0; i < 4; i++) {
+            if (i != 0) {
+                pattern = turnPatternToTheLeft(pattern);
+            }
+            patternTempHashes = new int[patternLength];
+            computeTempHashes(patternTempHashes, pattern, patternLength);
+            patternsHashes.add(computeHash(patternLength, patternTempHashes));
+            patterns.add(pattern);
+            pattern = reversePattern(pattern);
+            patternTempHashes = new int[patternLength];
+            computeTempHashes(patternTempHashes, pattern, patternLength);
+            patternsHashes.add(computeHash(patternLength, patternTempHashes));
+            patterns.add(pattern);
+        }
+
         int[] tempHashes = new int[columnsQuantity];
         computeTempHashes(tempHashes, text, patternLength);
-        int tempHash=computeHash(patternLength, tempHashes);
+        int tempHash = computeHash(patternLength, tempHashes);
         // further logic part
         for (int k = 0; k <= text.length - patternLength; k++) {
             // add some shift up and down logic for recomputing hashes
-            if (k!=0) {
-            
-                //recomputing temHashes array
+            if (k != 0) {
+
+                // recomputing temHashes array
                 for (int i = 0; i < tempHashes.length; i++) {
-                    tempHashes[i]-=text[k-1][i]*Math.pow(SIGMA, patternLength-1);
-                    tempHashes[i]*=SIGMA;
-                    tempHashes[i]+=text[k+patternLength-1][i];
+                    tempHashes[i] -= text[k - 1][i]
+                            * Math.pow(SIGMA, patternLength - 1);
+                    tempHashes[i] *= SIGMA;
+                    tempHashes[i] += text[k + patternLength - 1][i];
                 }
-                tempHash=computeHash(patternLength, tempHashes);
-                       
-            
-            } 
-            
-            
-            
+                tempHash = computeHash(patternLength, tempHashes);
+
+            }
+
             for (int i = 0; i <= text[0].length - patternLength; i++) {
                 if (i != 0) {
                     // recomputing hash
-                   
+
                     tempHash = ((tempHash - (tempHashes[i - 1] * (int) Math
                             .pow(SIGMA, patternLength - 1))) * SIGMA + tempHashes[i
                             + (patternLength - 1)])
                             % ourModulo;
                 }
-                if (tempHash == patternHash) {
+                if (patternsHashes.contains(tempHash)) {
                     // real equality checking
-                    boolean identity = true;
-                    outer: for (int j = 0; j < patternLength; j++) {
-                        for (int j2 = 0; j2 < patternLength; j2++) {
-                            if (pattern[j][j2] != text[j + k][j2 + i]) {
-                                identity = false;
-                                break outer;
+                    boolean identity = false; //initialisation
+                    for (char[][] patternsElement : patterns) {
+                        identity=true;
+                        outer: for (int j = 0; j < patternLength; j++) {
+                            for (int j2 = 0; j2 < patternLength; j2++) {
+                                if (patternsElement[j][j2] != text[j + k][j2 + i]) {
+                                    identity = false;
+                                    break outer;
+                                }
                             }
+                        }
+                        if (identity) {
+                            break;
                         }
                     }
                     if (identity) {
-                        System.out.println("I found pattern on position: [ " + k+ ", "+i+" ]");
+                        System.out.println("I found pattern on position: [ "
+                                + k + ", " + i + " ]");
                     } else {
                         falseCounter++;
                     }
@@ -109,39 +135,37 @@ public class EnginePremium {
         System.out.println("Fake alarms: " + falseCounter);
     }
 
-    private char[][] turnPatternToTheLeft(char[][] pattern)
-    {
-        char[][] newPattern=new char[pattern.length][pattern[0].length];
+    private char[][] turnPatternToTheLeft(char[][] pattern) {
+        char[][] newPattern = new char[pattern.length][pattern[0].length];
         for (int i = 0; i < pattern.length; i++) {
             for (int j = 0; j < pattern[0].length; j++) {
-                newPattern[i][j]=pattern[j][pattern.length-1-i];
+                newPattern[i][j] = pattern[j][pattern.length - 1 - i];
             }
         }
         return newPattern;
     }
-    
-    private char[][] reversePattern(char[][] pattern)
-    {
-        char[][] newPattern=new char[pattern.length][pattern[0].length];
+
+    private char[][] reversePattern(char[][] pattern) {
+        char[][] newPattern = new char[pattern.length][pattern[0].length];
         for (int i = 0; i < pattern.length; i++) {
             for (int j = 0; j < pattern[0].length; j++) {
-                newPattern[i][j]=pattern[i][pattern[0].length-1-j];
+                newPattern[i][j] = pattern[i][pattern[0].length - 1 - j];
             }
         }
         return newPattern;
     }
-    
+
     public static void main(String[] args) {
         EnginePremium engine = new EnginePremium(Integer.MAX_VALUE / 2);
         char[][] text = { { '1', '1', '2' }, { '3', '3', '4' },
-                { '1', '2', '5' }, { '3', '4', '5' } };
+                { '2', '4', '2' }, { '1', '3', '1' } };
 
-        char[][] pattern = { { '1', '2' ,'3'}, { '4', '5' , '6'}, {'7', '8', '9'} };
-        
-       // char[][] pattern = { { '3', '3' }, { '1', '2' } };
+        char[][] pattern = { { '1', '2' }, { '3', '4'}};
 
-        //engine.searchForPattern(text, pattern);
-       // engine.reversePattern(pattern);
+        // char[][] pattern = { { '3', '3' }, { '1', '2' } };
+
+        engine.searchForPattern(text, pattern);
+        // engine.reversePattern(pattern);
 
     }
 
